0e2bd15a25d4eae779154e7bf2cac05c8fe2a88b
pRxCSGO <50997909+pRxCSGO@users.noreply.github.com>
2019-06-18 13:27:43 +0300

    Add files via upload

---
+++ events/guildCreate.js
@@ -0,0 +1,21 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+const girismesaj = [
+  '**ANUBIS BOT sunucunuza eklendi!**',
+  '**ANUBIS BOT** sunucunuzdaki insanlara kolaylıklar sağlar.',
+  'Bot pRx tarafından geliştirilmektedir (https://twitch.tv/prxcstwitch)',
+  'Botumuzun özelliklerini öğrenmek için +yardım komutunu kullanabilirsin.',
+  '**ÖNEMLİ:** Botun kullanması için mod-log kanalı açın ve deneme için',
+  '+uyarı komutunu kullanın.',
+  '',
+  ]
+
+client.on('guildCreate', guild => {
+    const generalChannel = guild.defaultChannel
+    generalChannel.sendMessage(girismesaj)
+	client.user.setGame(prefix + 'yardım | ' + client.guilds.size + ' sunucu | ' + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' kullanıcı');
+})
---
+++ komutlar/sponsor.js
@@ -0,0 +1,25 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+var gis = require('g-image-search');
+
+exports.run = (client, message) => {
+message.channel.send({embed: {
+    color: 0xD97634,
+    title: ":tada: YOK",
+    url: "http://google.com",
+    description: "YOK",
+            }});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['p'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sponsor',
+  description: 'sponsorları gösterir.',
+  usage: 'sponsor'
+};
